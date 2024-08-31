6884bac100d9cc2476a061a3e884aae73155f5b6
John Name <oluwatobilobasalawu1234@gmail.com>
2024-08-31 18:50:36 +0100

    updated about me section

--- src/lib/content/about.ts
+++ src/lib/content/about.ts
@@ -1,19 +1,21 @@
-import { AboutSectionType } from '@/lib/types/sections';
+import { AboutSectionType} from '@/lib/types/sections';
 
 export const aboutSection: AboutSectionType = {
   title: 'about me',
   paragraphs: [
-    'I’m a passionate developer eager to explore every tech stack, especially in Big Data Technolgies and Artifical Intelligence.',
-    'My main focus these days is learning new technologies and finding a decent job.',
+    "I may not know much yet, but with the little knowledge I have, I've already built some impressive projects. My passion for learning and pushing the boundaries of what I can create drives me every day.",
+    'Recently, I completed an internship at Headstarter, where I created several projects that helped improve my skills. My current focus is on my Python expertise and expanding my software engineering stack.',
+    "When I'm not coding, you'll find me working on personal development or listening my favorite playlist."
   ],
   list: {
-    title: 'Here are a few languages I’ve been working with recently:',
+    title: 'Here are a few languages & frameworks I’ve been working with recently:',
     items: [
-      'C',
       'Python',
-      'C++',
-      'Bash',
-      'SQL',
+      'JavaScript',
+      'HTML/CSS',
+      'Firebase',
+      'NextJS',
+      'TailwindCSS'
     ],
   },
   img: '/img.jpg',
