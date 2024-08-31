eb2e08f854b786baeb2993fd95c5cf78e143ef2c
John Name <oluwatobilobasalawu1234@gmail.com>
2024-08-31 17:50:20 +0100

    updated seo main content

--- src/lib/content/portfolio.ts
+++ src/lib/content/portfolio.ts
@@ -7,27 +7,25 @@
 import { StringKeyValueType } from '../types';
 
 export const socialLinks: StringKeyValueType = {
-  facebook: 'https://www.facebook.com/talha.ramzan.3367',
-  instagram: 'https://instagram.com/o21tlha',
   twitter: 'https://twitter.com/c2tlhah',
   github: 'https://github.com/c2-tlhah',
   linkedin: 'https://www.linkedin.com/in/muhammad-talha-ramzan',
 };
 
 export const author = {
-  name: 'Muhammad Talha Ramzan',
-  email: 'c2.tlhah@gmail.com',
+  name: 'Oluwatobi Salawu',
+  email: 'tobisal.dev@gmail.com',
 };
 
 export const seoData = {
-  title: 'Muhammad Talha | Portfolio',
+  title: 'Oluwatobi Salawu | Portfolio',
   description: 'Passionate about Data Engineering and Mahine Learning, focusing on creating innovative digital solutions.',
   author: author.name,
   image: 'https://i.postimg.cc/7hTb4LDz/Portfolio.png',
   url: 'https://vercel.app',
   keywords: [
-    'Talha',
-    'Muhammad Talha',
+    'Tobi',
+    'Oluwatobi Salawu',
     'Muhammad Talha Ramzan',
     '@o21tlha',
     'talha ramzan',
--- src/lib/content/social.ts
+++ src/lib/content/social.ts
@@ -9,20 +9,12 @@ export const socialSection: SocialSectionType = {
         url: socialLinks.github,
     },
     {
-        icon: 'mdi:instagram',
-        url: socialLinks.instagram,
-    },
-    {
         icon: 'lucide:twitter',
         url: socialLinks.twitter,
     },
     {
         icon: 'lucide:linkedin',
         url: socialLinks.linkedin,
-    },
-    {
-        icon: 'lucide:facebook',
-        url: socialLinks.facebook,
     },
     ],
 };
