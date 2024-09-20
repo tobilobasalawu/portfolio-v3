1409009ca33fa0e091a3acaa50408142011cc08a
John Name <oluwatobilobasalawu1234@gmail.com>
2024-09-20 20:56:08 +0100

    Adding analytics to webpage

--- src/containers/index.ts
+++ src/containers/index.ts
@@ -1,3 +1,6 @@
+'use client'
+
+import React, { useEffect } from 'react';
 import About from './About';
 import Contact from './Contact';
 import Email from './Email';
@@ -11,6 +14,26 @@ import Projects from './Projects';
 import Skills from './Skills';
 import Social from './Social';
 import SocialLinks from './Social/SocialLinks';
+
+const GoogleTag = () => {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = "https://www.googletagmanager.com/gtag/js?id=G-2PW52VZB98";
+    script.async = true;
+    document.head.appendChild(script);
+
+    const inlineScript = document.createElement('script');
+    inlineScript.innerHTML = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-2PW52VZB98');
+    `;
+    document.head.appendChild(inlineScript);
+  }, []);
+
+  return null; // This component does not render anything
+};
 
 export {
   About,
@@ -26,4 +49,5 @@ export {
   Skills,
   Social,
   SocialLinks,
+  GoogleTag, // Export the GoogleTag component
 };
