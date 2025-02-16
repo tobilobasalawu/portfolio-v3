b8fdbaae5bbe5dd82680a926ee197fd94d05ee25
Tobi <155114173+tobilobasalawu@users.noreply.github.com>
2025-02-16 01:36:34 +0000

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   reactStrictMode: true,
-  distDir: ".",
-  assetPrefix: "/", // for github pages
 };
 
 export default nextConfig;
