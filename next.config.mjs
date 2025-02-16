64b292e89ab5daa0245d942b419fb88dc72eda00
John Name <oluwatobilobasalawu1234@gmail.com>
2025-02-16 01:04:25 +0000

    updated error

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   reactStrictMode: true,
+  assetPrefix: "/", // for github pages
 };
 
 export default nextConfig;
