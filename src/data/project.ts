e78d63af3a476baae21c4482f0f8144425202107
Tobi <oluwatobilobasalawu1234@gmail.com>
2025-02-22 22:34:54 +0000

    feat: 2025 projects

--- src/data/project.ts
+++ src/data/project.ts
@@ -7,10 +7,33 @@ export type Project = {
 
 export const projects: Project[] = [
   {
-    year: "2023",
-    name: "Emerson Collective",
-    technologies: ["Next.js", "TypeScript", "SCSS", "Contentful"],
-    link: "https://emersoncollective.com",
+    year: "2025",
+    name: "UI-Royale",
+    technologies: ["Next.js", "Firebase", "ShadcnUI", "MagicUI", "Superflex"],
+    link: "https://ui-royale.vercel.app/",
   },
-  // Add more projects here...
+  {
+    year: "2025",
+    name: "WApprentice",
+    technologies: ["Next.js", "Firebase", "ShadcnUI", "TailwindCSS"],
+    link: "https://www.wapprentice.app/",
+  },
+  {
+    year: "2025",
+    name: "NoBeesWax",
+    technologies: ["Clerk", "Firebase", "Vite", "Ollama",'Meta-AI', "Reddit API"],
+    link: "https://github.com/tobilobasalawu/NoBeesWax",
+  },
+  {
+    year: "2025",
+    name: "Brainrot-ChatBot",
+    technologies: ["Python", "Firebase", "Streamlit", "OpenAI", "Flask"],
+    link: "https://github.com/tobilobasalawu/brainrot-chat-bot",
+  },
+  {
+    year: "2025",
+    name: "Personal Website V3",
+    technologies: ["Next.js", "ShadcnUI", "MagicUI", "Motion", "TailwindCSS"],
+    link: "https://oluwatobisalawu.me/",
+  },
 ];
